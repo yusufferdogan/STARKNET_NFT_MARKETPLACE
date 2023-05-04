902fbd3b16de221a8f6cdbfe8a99d63a042820ff
yusufferdogan <yusuffx03@gmail.com>
2023-05-04 19:54:59 +0300

    custom checkbox & responsive

--- pages/collection/components/dropDown.js
+++ pages/collection/components/dropDown.js
@@ -1,47 +1,73 @@
-import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
+import { BsChevronDown, BsChevronUp, BsCheckLg } from 'react-icons/bs';
 import { useState } from 'react';
-export function Dropdown() {
+export function Dropdown({ name, options }) {
   const [isOpen, setIsOpen] = useState(false);
-  const options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-
   return (
-    <div className="w-96">
+    <div className="py-2">
       <button
         type="button"
-        className="w-80"
+        className="max-w-xs md:max-w-none hidden lg:block 2xl:w-80 xl:w-72 lg:w-40 md:w-32"
         id="options-menu"
         onClick={() => setIsOpen(!isOpen)}
         aria-haspopup="true"
         aria-expanded="true"
       >
         <div className="flex justify-between">
-          <p>Options</p>
+          <p>{name}</p>
           <div className="flex gap-2 items-center">
-            <p> 16</p>
-            <BsChevronDown size={22} />
+            <p>{options.length}</p>
+            {isOpen ? <BsChevronUp size={22} /> : <BsChevronDown size={22} />}
           </div>
         </div>
       </button>
       {isOpen && (
         <div
-          className="m-5 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
+          className="pt-2"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="options-menu"
         >
           {options.map((option, index) => (
-            <div key={index} className="p-1" role="none">
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-white"
-                role="menuitem"
-              >
-                {option}
-              </a>
+            <div
+              key={index}
+              className="flex items-center justify-between p-1"
+              role="none"
+            >
+              <div className="flex items-center gap-5">
+                <CheckBox></CheckBox>
+                <p> {option.value}</p>
+              </div>
+              <p>{option.count}</p>
             </div>
           ))}
         </div>
       )}
+    </div>
+  );
+}
+
+function CheckBox() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div className="flex items-center mr-4">
+      <button
+        onClick={handleCheckboxChange}
+        id="orange-checkbox"
+        type="checkbox"
+        className="
+        w-6
+        h-6
+        border-2 border-gray-400
+        rounded-sm
+      "
+      >
+        {isChecked && <BsCheckLg className="w-4 h-4 mx-auto" />}
+      </button>
     </div>
   );
 }
