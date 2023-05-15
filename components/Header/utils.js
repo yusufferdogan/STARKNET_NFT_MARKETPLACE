08aee114120534a0a15d1a059101d785ad7dd53a
yusufferdogan <yusuffx03@gmail.com>
2023-05-15 22:48:23 +0300

    connect button

--- components/Header/utils.js
+++ components/Header/utils.js
@@ -33,7 +33,8 @@ export function OpenModal({ available, connect, connectors }) {
         id="popup-modal"
         tabIndex={-1}
         className=" fixed top-0 left-0 right-0 z-50 hidden p-4
-         overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+         overflow-x-hidden overflow-y-auto md:inset-0
+          h-[calc(100%-1rem)] max-h-full"
       >
         <Modal
           appElement={document.getElementById('body')}
@@ -146,7 +147,10 @@ export function Button({ key, text, onClick }) {
     <button
       key="{key}"
       type="button"
-      className="bg-white hover:bg-gray-200 text-gray-800  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      className="bg-white hover:bg-gray-200
+       text-gray-800 focus:ring-4 focus:outline-none
+         font-medium rounded-lg text-xs 
+         px-3 py-2 text-center"
       onClick={onClick}
     >
       Connect Wallet
